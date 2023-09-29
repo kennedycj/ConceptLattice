dd70bd4264c69748846ab68046214c93b8218e87
kennedycj <jacksonhaddock@gmail.com>
2023-09-28 19:47:21 -0500

    Add normalize and scale methods.

--- optimizer.py
+++ optimizer.py
@@ -1,28 +1,19 @@
 import pulp
 import pandas as pd
-import numpy as np
-from enum import Enum
 import portion as P
 import draw
 import matplotlib.pyplot as plt
 
-class Objective(Enum):
-    MIN_START = 1
-    MIN_END = 2
-    MIN_LENGTH = 3
-    MIN_DISTANCE = 4
 class Optimizer:
-    def __init__(self, lower_bound, upper_bound, objective=Objective['MIN_DISTANCE'], no_reverse=True, limit=1000):
+    def __init__(self, lower_bound, upper_bound, no_reverse=True, limit=1000):
         self.no_reverse = no_reverse
-        self.objective = objective
         self.M = limit
         self.intervals = {}
         self.N = 0
         self.overlap_index = 0
-        self.model = pulp.LpProblem(f"{self.objective.name}", pulp.LpMinimize)
+        self.model = pulp.LpProblem(f"Maximum distance", pulp.LpMinimize)
         self.L = lower_bound
         self.U = upper_bound
-        self.names = {}
         self.C = []
         self.z = []
         self.Z = 0
@@ -34,7 +25,7 @@ class Optimizer:
         if not name in self.intervals:
             self.intervals[name] = self.N
             self.C.extend([pulp.LpVariable(f"x_{i}", lowBound=self.L, upBound=self.U) for i in range(2 * self.N, 2 * self.N + 2)])
-            print(f"name = {name} C = {self.C}")
+            print(f"this = {name} C = {self.C}")
             self.N += 1
 
         if pd.notnull(start):
@@ -45,19 +36,18 @@ class Optimizer:
 
         if pd.notnull(length):
             self.model += self.C[2 * self.intervals[name] + 1] - self.C[2 * self.intervals[name]] == length
-    def add_interval_overlap(self, name, partner, length):
-        if not name in self.intervals or not partner in self.intervals:
+    def add_interval_overlap(self, this, that, length):
+        if not this in self.intervals or not that in self.intervals:
             raise TypeError('interval does not exist')
 
         self.z.extend([pulp.LpVariable(f"z_{i}", cat=pulp.LpBinary) for i in range(4 * self.Z, 4 * self.Z + 4)])
         self.model += pulp.lpSum([self.z[i] for i in range(4 * self.Z, 4 * self.Z + 4)]) == 1
 
-        print(f"{name} overlaps {partner} by {length}")
-
-        a_s = self.C[2 * self.intervals[name]]
-        a_e = self.C[2 * self.intervals[name] + 1]
-        b_s = self.C[2 * self.intervals[partner]]
-        b_e = self.C[2 * self.intervals[partner] + 1]
+        print(f"{this} overlaps {that} by {length}")
+        a_s = self.C[2 * self.intervals[this]]
+        a_e = self.C[2 * self.intervals[this] + 1]
+        b_s = self.C[2 * self.intervals[that]]
+        b_e = self.C[2 * self.intervals[that] + 1]
 
         # Constraints
         i = self.Z
@@ -91,6 +81,11 @@ class Optimizer:
 
         self.Z += 1
     def add_interval_order(self, first, second, distance, starts=False):
+        """
+        Add distance constraint between two intervals, first and second. Assumes first is before second. If starts is
+        True, ensure second.start is exactly distance (d) from first.start; otherwise, by default, second.start is
+        exactly distance (d) from first.end.
+        """
         if not first in self.intervals or not second in self.intervals:
             raise TypeError('interval does not exist')
 
@@ -102,30 +97,34 @@ class Optimizer:
             self.model += (b_s - a_s) == distance
         else:
             self.model += (b_s - a_e) == distance
-
-    def solve(self):
+    def solve(self, normalize=False, scale=False):
+        if scale:
+            normalize = True
         # By default, constrain intervals to have positive length: start <= end
         if self.no_reverse:
             for i in range(0, 2 * self.N, 2):
                 self.model += self.C[i] <= self.C[i + 1]
 
-        if self.objective == Objective.MIN_DISTANCE:
-            # Define the objective function to minimize the absolute difference
-            self.model += self.sum == pulp.lpSum([pulp.lpSum([self.C[i] - self.C[j]] for j in range(i, 2 * self.N)) for i in range(0, 2 * self.N)])
-
-        elif self.objective == Objective.MIN_START:
-            self.model += self.sum == pulp.lpSum([self.C[i] for i in range(0, 2 * self.N, 2)])
-        elif self.objective == Objective.MIN_END:
-            print(f"MIN_END")
-            self.model += self.sum == pulp.lpSum([(self.C[i] - self.L) for i in range(1, 2 * self.N, 2)])
-        elif self.objective == Objective.MIN_LENGTH:
-            self.model += self.sum == pulp.lpSum([(self.C[i + 1] - self.C[i]) for i in range(0, 2 * self.N, 2)])
-
+        # Define the objective function to minimize the absolute difference
+        self.model += self.sum == pulp.lpSum([pulp.lpSum([self.C[i] - self.C[j]] for j in range(i, 2 * self.N)) for i in range(0, 2 * self.N)])
         self.model += self.abs_sum >= self.sum
         self.model += self.abs_sum >= -self.sum
 
         # Solve the linear programming problem
         self.model.solve()
+
+        if normalize:
+            max_x = (max(self.C, key=lambda x: x.varValue)).varValue
+            min_x = (min(self.C, key=lambda x: x.varValue)).varValue
+
+            shift = 0
+            factor = 1
+            if scale:
+                shift = self.L
+                factor = self.U - self.L
+
+            for i in range(0, len(self.C)):
+                self.C[i].varValue = shift + factor * (self.C[i].varValue - min_x) / (max_x - min_x)
 
         # Print the results
         if pulp.LpStatus[self.model.status] == "Optimal":
@@ -144,12 +143,12 @@ class Optimizer:
 
     def interval_names(self):
         return sorted(self.intervals, key=lambda k: self.intervals[k])
-def optimize_from_file(filename, objective):
+def optimize_from_file(filename, scale=False):
     P = pd.read_excel(filename, sheet_name='Position')
     O = pd.read_excel(".\\Intervals.xlsx", sheet_name='Overlap')
-    opt = Optimizer(0, 10, objective=objective)
+    opt = Optimizer(0, 10)
     # Define the intervals start, end, and length constraints
-    names = opt.names
+    names = {}
     for i, row in P.iterrows():
         name = P.at[i, 'Name']
         names[P.at[i, 'Name']] = i
@@ -167,11 +166,11 @@ def optimize_from_file(filename, objective):
         if pd.notnull(length):
             opt.add_interval_overlap(name, partner, length)
 
-    opt.solve()
+    opt.solve(scale=scale)
     return opt
 
 if __name__ == '__main__':
-    opt = optimize_from_file(".\\Intervals.xlsx", Objective['MIN_END'])
+    opt = optimize_from_file(".\\Intervals.xlsx", scale=True)
     print(f"intervals = {opt.as_intervals()}")
     # Plot
     draw.graph(RM=opt.as_intervals(), names=opt.interval_names(), lw=5, c='b')
