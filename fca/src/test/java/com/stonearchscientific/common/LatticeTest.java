9e0ee9acd67693f5e2d023f07e55f009a6a3c262
kennedycj <jacksonhaddock@gmail.com>
2024-02-05 22:35:52 -0600

    Use more robust adjacency matrix comparison to test Lattice.addIntent(...).

--- fca/src/test/java/com/stonearchscientific/common/LatticeTest.java
+++ fca/src/test/java/com/stonearchscientific/common/LatticeTest.java
@@ -23,24 +23,23 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LatticeTest {
-    private Graph toy, graph;
+    private Graph graph;
     List<String> objects, attributes;
     Lattice<BitSet, BitSet> lattice;
 
+    int[][] expected = {
+        {0, 1, 0, 1, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 1, 0, 1},
+        {0, 0, 0, 0, 1, 1, 0, 0},
+        {1, 0, 0, 0, 0, 0, 1, 1},
+        {0, 0, 1, 0, 0, 0, 1, 1},
+        {0, 1, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 1, 0, 0, 0},
+        {0, 1, 0, 1, 1, 0, 0, 0}
+    };
+
     private BitSet bitset(String bitstring) {
         return BitSet.valueOf(new long[]{Long.parseLong(bitstring, 2)});
-    }
-
-    private Vertex createVertex(Graph graph, Concept<BitSet, BitSet> concept) {
-        Vertex vertex = graph.addVertex(null);
-        vertex.setProperty("label", concept);
-        vertex.setProperty("color", 0);
-        return vertex;
-    }
-
-    private void createEdge(Graph graph, Vertex v1, Vertex v2) {
-        graph.addEdge(null, v1, v2, "");
-        graph.addEdge(null, v2, v1, "");
     }
 
     private int numberOfVertices(Graph graph) {
@@ -61,67 +60,40 @@ public class LatticeTest {
 
     @Before
     public void setUp() {
-        toy = new TinkerGraph();
         graph = new TinkerGraph();
-
         objects = Arrays.asList("1", "2", "3", "4", "5");
         attributes = Arrays.asList("a", "b", "c", "d", "e");
-
-        Concept<BitSet, BitSet> c1 = new Concept<>(bitset("00001"), bitset("11111"));
-        Concept<BitSet, BitSet> c2 = new Concept<>(bitset("00011"), bitset("10111"));
-        Concept<BitSet, BitSet> c3 = new Concept<>(bitset("00101"), bitset("01100"));
-        Concept<BitSet, BitSet> c4 = new Concept<>(bitset("01011"), bitset("10000"));
-        Concept<BitSet, BitSet> c5 = new Concept<>(bitset("11111"), bitset("00000"));
-        Concept<BitSet, BitSet> c6 = new Concept<>(bitset("00111"), bitset("00100"));
-
-        Vertex v1 = createVertex(graph, c1);
-        Vertex v2 = createVertex(graph, c2);
-        Vertex v3 = createVertex(graph, c3);
-        Vertex v4 = createVertex(graph, c4);
-        Vertex v5 = createVertex(graph, c5);
-        Vertex v6 = createVertex(graph, c6);
-
-        createEdge(graph, v2, v1);
-        createEdge(graph, v3, v1);
-        createEdge(graph, v4, v2);
-        createEdge(graph, v6, v2);
-        createEdge(graph, v6, v3);
-        createEdge(graph, v5, v4);
-        createEdge(graph, v5, v6);
     }
 
     @Test
     public void testSetup() {
-        assertEquals(numberOfVertices(graph), 6);
-        assertEquals(numberOfEdges(graph), 14);
+        assertEquals(numberOfVertices(graph), 0);
+        assertEquals(numberOfEdges(graph), 0);
     }
     @Test
     public void testAddIntent() {
-        lattice = new Lattice<>(toy, new Concept<>(bitset("00001"), bitset("11111")));
-        lattice.insert(toy, new Concept<>(bitset("00010"), bitset("10111")));
-        lattice.insert(toy, new Concept<>(bitset("00100"), bitset("01100")));
-        lattice.insert(toy, new Concept<>(bitset("01000"), bitset("10000")));
-        lattice.insert(toy, new Concept<>(bitset("10000"), bitset("01111")));
+        lattice = new Lattice<>(graph, new Concept<>(bitset("00001"), bitset("11111")));
+        lattice.insert(graph, new Concept<>(bitset("00010"), bitset("10111")));
+        lattice.insert(graph, new Concept<>(bitset("00100"), bitset("01100")));
+        lattice.insert(graph, new Concept<>(bitset("01000"), bitset("10000")));
+        lattice.insert(graph, new Concept<>(bitset("10000"), bitset("01111")));
 
-        String graphvizOutput = Main.graphviz(toy, objects, attributes);
+        String graphvizOutput = Main.graphviz(graph, objects, attributes);
 
         try {
             MutableGraph g = new Parser().read(graphvizOutput);
-            Graphviz.fromGraph(g).width(700).render(Format.PNG).toFile(new File("LatticeTestAddIntent_INTERIM.png"));
+            Graphviz.fromGraph(g).width(700).render(Format.PNG).toFile(new File("LatticeTestAddIntent.png"));
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        int[][] matrix = Main.generateAdjacencyMatrix(toy);
+        int[][] observed = Main.generateAdjacencyMatrix(graph);
 
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+        for(int i = 0; i < observed.length; i++) {
+            for(int j = 0; j < observed[i].length; j++) {
+                assertEquals(expected[i][j], observed[i][j]);
             }
             System.out.println();
         }
-
-        System.out.println("Number of vertices: " + numberOfVertices(toy));
-        System.out.println("Number of edges: " + numberOfEdges(toy));
     }
 }
